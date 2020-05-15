78189187f02eb19180e4caf5beabff1405b9bb64
valefaraz <valefaraz@gmail.com>
2020-05-15 17:49:38 -0300

    cajero

--- 58018-Faraz-Valentin/tp2/cajero.py
+++ 58018-Faraz-Valentin/tp2/cajero.py
@@ -1,6 +1,21 @@
 from billetes import *
 import math
 
+class MultiploError(Exception):
+    pass
+
+class DineroInsuficienteError(Exception):
+    pass
+
+class CombinacionDeBilletesError(Exception):
+    pass
+
+class RangoPorcentajeError(Exception):
+    pass
+
+class ExtraccionNegativaError(Exception):
+    pass
+
 class Cajero():
 
     def __init__(self):
@@ -10,8 +25,9 @@ class Cajero():
         self.billetes_cien = []
         self.total = 0
         self.extraccion = []
+        self.billetes_extraidos=""
 
-    def agregar_dinero(self,lista_billetes):
+    def agregar_dinero(self, lista_billetes):
         for x in lista_billetes:
             if x.denominacion == 100:
                 self.billetes_cien.append(x)
@@ -26,7 +42,7 @@ class Cajero():
         billetes =[len(self.billetes_cien), len(self.billetes_doscientos),
                     len(self.billetes_quinientos), len(self.billetes_mil)]
         self.total = (billetes[0]*100 + billetes[1]*200 + billetes[2]*500 + billetes[3]*1000)
-        #print(self.total)
+        #print("Total disponible en el cajero:", self.total)
         billetes_contados=[]
         for x in range(len(billetes)):
             if x == 0:
@@ -39,128 +55,138 @@ class Cajero():
                 valor = 1000
             string = (str(billetes[x]) + " billetes de $" + str(valor))
             billetes_contados.append(string)
-            #print(billetes[x],"de $",valor)
-        #print("Total: $",self.total)
-        #return(billetes_contados)
         return(", ".join(billetes_contados))
 
-    def extraer_dinero(self,monto):
+    def extraer_dinero(self, monto):
         disponible =[len(self.billetes_cien), len(self.billetes_doscientos),
                     len(self.billetes_quinientos), len(self.billetes_mil)]
-        contador = 0
-        if monto%100 != 0:
-            return("El monto no es multiplo de 100")
-        if monto > self.total:
-            return("No hay suficiente dinero")
-        #self.extraccion = []
-        while True:
-            while monto >= 1000 and disponible[3] != 0:
-                disponible[3] -= 1
-                contador += 1000
-                monto -= 1000
-                self.extraccion.append("$1000")
-            while monto >= 500 and disponible[2] != 0:
-                disponible[2] -= 1
-                contador += 500
-                monto -= 500
-                self.extraccion.append("$500")
-            while monto >= 200 and disponible[1] != 0:
-                disponible[1] -= 1
-                contador += 200
-                monto -= 200
-                self.extraccion.append("$200")
-            while monto >= 100 and disponible[0] != 0:
-                disponible[0] -= 1
-                contador += 100
-                monto -= 100
-                self.extraccion.append("$100")
-            if monto == 0:
-                break
-            else:
-                self.extraccion = []
-                return("Error. No hay una combinación de billetes que nos permita extraer ese monto.")
+        try:
+            if monto%100 != 0:
+                raise MultiploError("El monto no es multiplo de 100")
+
+            if monto > self.total:
+                raise DineroInsuficienteError("No hay suficiente dinero")
+            
+            if int(monto) < 0:
+                raise ExtraccionNegativaError("El monto a retirar no puede ser negativo")
+            
+            contador = 0
+            while True:
+                while monto >= 1000 and disponible[3] != 0:
+                    disponible[3] -= 1
+                    contador += 1000
+                    monto -= 1000
+                    self.extraccion.append("$1000")
+                while monto >= 500 and disponible[2] != 0:
+                    disponible[2] -= 1
+                    contador += 500
+                    monto -= 500
+                    self.extraccion.append("$500")
+                while monto >= 200 and disponible[1] != 0:
+                    disponible[1] -= 1
+                    contador += 200
+                    monto -= 200
+                    self.extraccion.append("$200")
+                while monto >= 100 and disponible[0] != 0:
+                    disponible[0] -= 1
+                    contador += 100
+                    monto -= 100
+                    self.extraccion.append("$100")
+                if monto == 0:
+                    break
+                else:
+                    self.extraccion = []
+                    raise CombinacionDeBilletesError("No hay billetes para esa combinacion de dinero")
+        except TypeError:
+            return("Error, debe ingresar un numero")
         veces100 = str(self.extraccion.count("$100"))+" billetes de $100, "
         veces200 = str(self.extraccion.count("$200"))+" billetes de $200, "
         veces500 = str(self.extraccion.count("$500"))+" billetes de $500, "
         veces1000 = str(self.extraccion.count("$1000"))+" billetes de $1000"
-        billetes_extraidos=""
         if veces100 != 0:
-            billetes_extraidos += veces100
+            self.billetes_extraidos += veces100
         if veces200 != 0:
-            billetes_extraidos += veces200
+            self.billetes_extraidos += veces200
         if veces500 != 0:
-            billetes_extraidos += veces500
+            self.billetes_extraidos += veces500
         if veces1000 != 0:
-            billetes_extraidos += veces1000
+            self.billetes_extraidos += veces1000
         self.total = (self.total - contador)
-        return(billetes_extraidos)
-    
+        return(self.billetes_extraidos)
+        
     def extraer_dinero_cambio(self, monto, porc):
         disponible =[len(self.billetes_cien), len(self.billetes_doscientos),
                     len(self.billetes_quinientos), len(self.billetes_mil)]
-        if porc > 100 or porc < 0:
-            return("Error. El porcentaje es incorrecto")
-        if monto > self.total:
-            return("No hay suficiente dinero")
-        if monto%100 != 0:
-            return("El monto no es multiplo de 100")
-        porc = math.trunc(monto * porc / 100)
-        sacar = porc
-        sacar = str(sacar)[::-1]
-        if len(sacar) <= 2:
-            sacar = 100
-        else:
-            sacar = sacar[::-1][:2]
-            sacar = (int(sacar[::-1]) + 1) * 100
-        if sacar - porc >= 100 or sacar - porc <= 0:
+        try:
+            if porc > 100 or porc < 0:
+                raise RangoPorcentajeError("El porcentaje debe ser entre 0 y 100")
+
+            if monto > self.total:
+                raise DineroInsuficienteError("No hay suficiente dinero")
+
+            if monto%100 != 0:
+                raise MultiploError("El monto no es multiplo de 100")
+
+            porc = math.trunc(monto * porc / 100)
             sacar = porc
-        self.extraccion = []
-        sin_cambio = monto - sacar
-        if self.extraer_dinero(sin_cambio) == "Error. No hay una combinación de billetes que nos permita extraer ese monto.":
-            sacar = 1000
-            sin_cambio = 0
-        sin_cambio = monto - sacar
-        #self.extraer_dinero(sin_cambio)
-        total_billetes_cambio = (len(self.billetes_cien)*100 + len(self.billetes_doscientos)*200
-                                + len(self.billetes_quinientos)*500)
-        while sacar <  total_billetes_cambio or (sacar - total_billetes_cambio)%100 == 0:
-            while sacar >= 100 and disponible[0] != 0:
-                disponible[0] -= 1
-                self.total -= 100
-                sacar -= 100
-                resto = sacar %200
-                if resto == 0:
-                    disponible[0] = 0
-                self.extraccion.append("$100")
-            while sacar >= 200 and disponible[1] != 0:
-                disponible[1] -= 1
-                self.total -= 200
-                sacar -= 200
-                resto = sacar %500
-                if resto == 0:
-                    disponible[1] = 0
-                self.extraccion.append("$200")
-            while sacar >= 500 and disponible[2] != 0:
-                disponible[2] -= 1
-                self.total -= 500
-                sacar -= 500
-                resto = sacar %1000
-                if resto == 0:
-                    disponible[2] = 0
-                self.extraccion.append("$500")
-            if sacar == 0:
-                break
+            sacar = str(sacar)[::-1]
+            if len(sacar) <= 2:
+                sacar = 100
             else:
-                break
-        while sacar >= 1000 and disponible[3] != 0:
-                disponible[3] -= 1
-                self.total -= 1000
-                sacar -= 1000
-                self.extraccion.append("$1000")
+                sacar = sacar[::-1][:2]
+                sacar = (int(sacar[::-1]) + 1) * 100
+            if sacar - porc >= 100 or sacar - porc <= 0:
+                sacar = porc
 
-        #return(self.extraer_dinero(sin_cambio),"cambio",self.extraccion)
-        return(self.extraer_dinero(sin_cambio))
-
+            self.extraccion = []
+            total_billetes_cambio = (len(self.billetes_cien)*100 + len(self.billetes_doscientos)*200
+                                    + len(self.billetes_quinientos)*500)
+            if sacar == 100 and disponible[0] == 0:
+                sacar = 200 
+            elif sacar >= 200 and sacar <= 400 and disponible[1] == 0 and disponible[0] == 0:
+                sacar = 500
+            elif sacar >= 500 and sacar <= 900:
+                if disponible[1] == 0 or disponible[0] == 0:
+                    sacar = 1000
+            sin_cambio = monto -sacar
+            while sacar <  total_billetes_cambio or (sacar - total_billetes_cambio)%100 == 0:
+                while sacar >= 100 and disponible[0] != 0:
+                    disponible[0] -= 1
+                    self.total -= 100
+                    sacar -= 100
+                    #resto = sacar %200
+                    #if resto == 0:
+                    #    disponible[0] = 0
+                    self.extraccion.append("$100")
+                while sacar >= 200 and disponible[1] != 0:
+                    disponible[1] -= 1
+                    self.total -= 200
+                    sacar -= 200
+                    #resto = sacar %500
+                    #if resto == 0:
+                    #    disponible[1] = 0
+                    self.extraccion.append("$200")
+                while sacar >= 500 and disponible[2] != 0:
+                    disponible[2] -= 1
+                    self.total -= 500
+                    sacar -= 500
+                    #resto = sacar %1000
+                    #if resto == 0:
+                    #    disponible[2] = 0
+                    self.extraccion.append("$500")
+                if sacar == 0:
+                    break
+                else:
+                    break
+            while sacar >= 1000 and disponible[3] != 0:
+                    disponible[3] -= 1
+                    self.total -= 1000
+                    sacar -= 1000
+                    self.extraccion.append("$1000")
+            return(self.extraer_dinero(sin_cambio))
+        except TypeError:
+            return("Error, el dinero y/o el porcentaje deben ser numeros")
+            
 if __name__ == "__main__":
     cajero = Cajero()
 
@@ -168,6 +194,7 @@ if __name__ == "__main__":
     billete200 = BilleteDe200()
     billete500 = BilleteDe500()
     billete1000 = BilleteDe1000()
+
     cajero.agregar_dinero(
                             [billete1000,billete1000,billete1000,
                             billete1000,billete1000,billete1000,
@@ -178,15 +205,13 @@ if __name__ == "__main__":
                             billete500,billete500,billete500,billete500,billete500,
                             billete500,billete500,billete500,billete500,billete500,
                             billete500,billete500,billete500,billete500,billete500])
-                        
-                            
-    print(cajero.contar_dinero())
-    #print(cajero.extraer_dinero(5000))
-    #print("1° extraccion:")
-    #print(cajero.extraer_dinero(1800))
-    #print("2° extraccion:")
-    print(cajero.extraer_dinero(3500))
-    #print("3° extraccion:")
-    #print(cajero.extraer_dinero_cambio(9000,10))
-    #print("4° extraccion:")
-    print(cajero.extraer_dinero_cambio(7000,10))
+
+    billetes_disponibles = cajero.contar_dinero()
+    print("Billetes disponibles:", billetes_disponibles)
+    print("Dinero total disponible:", cajero.total)
+    extraccion = cajero.extraer_dinero(1000)
+    print("Se extrajo del cajero:",extraccion)
+    #print("Dinero total disponible despues de la extraccion:", cajero.total)
+    
+    #extraccion_con_cambio = cajero.extraer_dinero_cambio(7000,10)
+    #print("Se extrajo del cajero", extraccion_con_cambio)
--- 58018-Faraz-Valentin/tp2/test_cajero.py
+++ 58018-Faraz-Valentin/tp2/test_cajero.py
@@ -10,6 +10,7 @@ class TestCajero(unittest.TestCase):
         self.billete500 = BilleteDe500()
         self.billete1000 = BilleteDe1000()
         self.cajero = Cajero()
+
         self.carga1 = (
                         [self.billete1000,self.billete1000,self.billete1000,
                         self.billete1000,self.billete1000,self.billete1000,
@@ -53,10 +54,6 @@ class TestCajero(unittest.TestCase):
         extraccion = self.cajero.extraer_dinero(5000)
         self.assertEqual(extraccion, ("0 billetes de $100, 0 billetes de $200"
                                     ", 0 billetes de $500, 5 billetes de $1000"))
-        extraccion1 = self.cajero.extraer_dinero(12000)
-        self.assertEqual(extraccion1, ("No hay suficiente dinero"))
-        extraccion2 = self.cajero.extraer_dinero(5520)
-        self.assertEqual(extraccion2, ("El monto no es multiplo de 100"))
     
     def test_2_a_b(self):
         self.cajero.agregar_dinero(self.carga2)
@@ -81,8 +78,6 @@ class TestCajero(unittest.TestCase):
         contar = self.cajero.contar_dinero()
         self.assertEqual(contar, ("0 billetes de $100, 0 billetes de $200"
                                 ", 20 billetes de $500, 10 billetes de $1000"))
-        extraccion1 = self.cajero.extraer_dinero(12100)
-        self.assertEqual(extraccion1, ("Error. No hay una combinación de billetes que nos permita extraer ese monto."))
         extraccion_cambio = self.cajero.extraer_dinero_cambio(7000,10)
         self.assertEqual(extraccion_cambio,("0 billetes de $100, 0 billetes de $200"
                                     ", 2 billetes de $500, 6 billetes de $1000"))
@@ -110,8 +105,6 @@ class TestCajero(unittest.TestCase):
         contar = self.cajero.contar_dinero()
         self.assertEqual(contar, ("0 billetes de $100, 15 billetes de $200"
                                 ", 20 billetes de $500, 10 billetes de $1000"))
-        extraccion2 = self.cajero.extraer_dinero(12100)
-        self.assertEqual(extraccion2, ("Error. No hay una combinación de billetes que nos permita extraer ese monto."))
     
     def test_3_e(self):
         self.cajero.agregar_dinero(self.carga3)
@@ -119,6 +112,73 @@ class TestCajero(unittest.TestCase):
         extraccion1 = self.cajero.extraer_dinero_cambio(7000, 10)
         self.assertEqual(extraccion1, ("0 billetes de $100, 5 billetes de $200"
                                 ", 0 billetes de $500, 6 billetes de $1000"))
+
+    def test_MultiploError(self):
+        self.cajero.agregar_dinero(self.carga1)
+        self.cajero.contar_dinero()
+        with self.assertRaises(MultiploError):
+            self.cajero.extraer_dinero(50)
+
+    def test_DineroInsuficienteError1(self):
+        self.cajero.agregar_dinero(self.carga1)
+        self.cajero.contar_dinero()
+        with self.assertRaises(DineroInsuficienteError):
+            self.cajero.extraer_dinero(100000)
+
+    def test_DineroInsuficienteError2(self):
+        self.cajero.agregar_dinero(self.carga1)
+        self.cajero.contar_dinero()
+        with self.assertRaises(DineroInsuficienteError):
+            self.cajero.extraer_dinero_cambio(1000000,10)
+    
+    def test_TypeError(self):
+        self.cajero.agregar_dinero(self.carga1)
+        self.cajero.contar_dinero()
+        error = self.cajero.extraer_dinero("mil pesos")
+        self.assertEqual(error, "Error, debe ingresar un numero")
+
+    def test_TypeError_con_cambio(self):
+        self.cajero.agregar_dinero(self.carga1)
+        self.cajero.contar_dinero()
+        error = self.cajero.extraer_dinero_cambio("mil pesos", 10)
+        self.assertEqual(error, "Error, el dinero y/o el porcentaje deben ser numeros")
+    
+    def test_TypeError_con_cambio2(self):
+        self.cajero.agregar_dinero(self.carga1)
+        self.cajero.contar_dinero()
+        error = self.cajero.extraer_dinero_cambio(1000, "diez porciento")
+        self.assertEqual(error, "Error, el dinero y/o el porcentaje deben ser numeros")
+    
+    def test_TypeError_con_cambio3(self):
+        self.cajero.agregar_dinero(self.carga1)
+        self.cajero.contar_dinero()
+        error = self.cajero.extraer_dinero_cambio("mil pesos", "diez porciento")
+        self.assertEqual(error, "Error, el dinero y/o el porcentaje deben ser numeros")
+
+    def test_RangoPorcentajeError(self):
+        self.cajero.agregar_dinero(self.carga1)
+        self.cajero.contar_dinero()
+        with self.assertRaises(RangoPorcentajeError):
+            self.cajero.extraer_dinero_cambio(1000,101)
+    
+    def test_CombinacionDeBilletesError(self):
+        self.cajero.agregar_dinero(self.carga2)
+        self.cajero.contar_dinero()
+        with self.assertRaises(CombinacionDeBilletesError):
+            self.cajero.extraer_dinero(12100)
+
+    def test_CombinacionDeBilletesError2(self):
+        self.cajero.agregar_dinero(self.carga3)
+        self.cajero.contar_dinero()
+        with self.assertRaises(CombinacionDeBilletesError):
+            self.cajero.extraer_dinero(12100)
+
+    def test_CombinacionDeBilletesError3(self):
+        self.cajero.agregar_dinero(self.carga2)
+        self.cajero.contar_dinero()
+        with self.assertRaises(CombinacionDeBilletesError):
+            self.cajero.extraer_dinero(900)
+    
 
 if __name__ == "__main__":
     unittest.main()
